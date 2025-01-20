b8e21f4539a59eee621ebf127e76a07daf42429d
Paulo Lagoá <paulo.lagoa@hitachivantara.com>
2025-01-20 15:26:16 +0000

    Update image.js

--- image.js
+++ image.js
@@ -1,4 +1,3 @@
-// Function to convert Blob to Image
 function blobToImage(blob) {
   return new Promise((resolve, reject) => {
     const img = new Image();
@@ -18,56 +17,47 @@ function blobToImage(blob) {
   });
 }
 
-
 function init() {
   const canvas = document.getElementById("mycanvas");
   const context = canvas.getContext("2d");
 
-  // Assuming `blob` is your image Blob
-  const blob = /* Your image Blob here */;
-  
-  // Convert the Blob to an image and draw it on the canvas
-  blobToImage(blob)
-    .then((image) => {
-      // Function to draw the image on the canvas at specified positions
-      const draw = function (fromX, fromY, toX, toY) {
-        context.drawImage(image, fromX, fromY, 210, 300, toX, toY, 210, 300);
-      };
+  var image = new Image();
+  image.src = “”;
 
-      const l1 = 0;
-      const l2 = 300;
-      const l3 = 600;
-      const l4 = 900;
-      const c1 = 0;
-      const c2 = 210;
-      const c3 = 420;
-      const c4 = 630;
+  const draw = function (fromX, fromY, toX, toY) {
+    context.drawImage(image, fromX, fromY, 210, 300, toX, toY, 210, 300);
+  };
 
-      // Draw the image at the desired positions
-      draw(c1, l1, c1, l1);
-      draw(c2, l1, c1, l2);
-      draw(c3, l1, c1, l3);
-      draw(c4, l1, c1, l4);
+  const l1 = 0;
+  const l2 = 300;
+  const l3 = 600;
+  const l4 = 900;
+  const c1 = 0;
+  const c2 = 210;
+  const c3 = 420;
+  const c4 = 630;
 
-      draw(c1, l2, c2, l1);
-      draw(c2, l2, c2, l2);
-      draw(c3, l2, c2, l3);
-      draw(c4, l2, c2, l4);
+  image.onload = function () {
+    draw(c1, l1, c1, l1);
+    draw(c2, l1, c1, l2);
+    draw(c3, l1, c1, l3);
+    draw(c4, l1, c1, l4);
 
-      draw(c1, l3, c3, l1);
-      draw(c2, l3, c3, l2);
-      draw(c3, l3, c3, l3);
-      draw(c4, l3, c3, l4);
+    draw(c1, l2, c2, l1);
+    draw(c2, l2, c2, l2);
+    draw(c3, l2, c2, l3);
+    draw(c4, l2, c2, l4);
 
-      draw(c1, l4, c4, l1);
-      draw(c2, l4, c4, l2);
-      draw(c3, l4, c4, l3);
-      draw(c4, l4, c4, l4);
-    })
-    .catch((error) => {
-      console.error("Error loading image:", error);
-    });
+    draw(c1, l3, c3, l1);
+    draw(c2, l3, c3, l2);
+    draw(c3, l3, c3, l3);
+    draw(c4, l3, c3, l4);
+
+    draw(c1, l4, c4, l1);
+    draw(c2, l4, c4, l2);
+    draw(c3, l4, c4, l3);
+    draw(c4, l4, c4, l4);
+  };
 }
 
-// Make sure this is run after the window loads
-window.onload = init;
+window.onload = init();
